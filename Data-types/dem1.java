2454173aa545a81da33251829cfa87815959ab80
Muskan Gautam <muskangautam7064@gmail.com>
2025-07-21 21:32:59 +0530

    first commit

---
+++ Data-types/dem1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class dem1{
+    public static void main(String args[]){
+       int firstDataType = 85;
+       System.out.println("my fisrt number is = " + firstDataType);
+
+       float secondDataType = 3;
+       System.out.println("my second number is = " + secondDataType);
+
+       double thirdDataType = 4.5;
+       System.out.println("my third number is = " + thirdDataType);
+
+       char myCharDataType = 'A';
+       System.out.println("my fourth number is = " + myCharDataType);
+
+       String text = "Hello World!";
+       System.out.println("My String is = " + text);
+      
+    }
+}
